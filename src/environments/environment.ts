f962f230e800c811f5b9406245a757bcf8a4b2c1
Maty Moyano <matymoyano4@gmail.com>
2024-08-01 20:52:51 -0300

    Last Changes

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    emailJsServiceId: 'service_xja6d3s',
+    emailJsTemplateId: 'template_2fm9jy4',
+    emailJsUserId: 'iAh-giIpQC6nJlijX'
+};
